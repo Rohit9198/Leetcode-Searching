6d016f1d18f4e00f91747930c0ef6332d25116e7
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-19 17:01:56 +0530

    ArrangeCoins

--- src/Searching/ArrangingCoins.java
+++ src/Searching/ArrangingCoins.java
@@ -2,6 +2,24 @@ package Searching;
 
 public class ArrangingCoins {
     public static void main(String[] args) {
+      int n=8;
+        System.out.println(arrangeCoin(n));
+    }
+    public static int arrangeCoin(int n){
+        long start = 0;
+        long end = n;
+        while(start <= end){
+            long mid = start +(end - start)/2;
 
+            long coinsRequired = mid * ( mid -1);
+            if(coinsRequired == n){
+                return (int) mid;
+            }else if (coinsRequired < n){
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        return (int)end;
     }
 }
