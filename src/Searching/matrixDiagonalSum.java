63948fdae564a970ba8ea3a962a19c1ee2197701
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-20 00:47:38 +0530

    diagonal matrix sum

---
+++ src/Searching/matrixDiagonalSum.java
@@ -0,0 +1,27 @@
+package Searching;
+
+public class matrixDiagonalSum {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        int answer = diagonalSum(matrix);
+        System.out.println(answer);
+    }
+    public static int diagonalSum(int[][] mat) {
+        int n = mat.length;
+        int sum =0;
+
+        for(int i=0; i< n; i++){
+            sum += mat[i][i];
+
+            if(i != n-1-i){
+                sum += mat[i][n-1-i];
+            }
+
+        }
+        return sum;
+    }
+}
