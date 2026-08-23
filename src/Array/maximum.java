b78dbf8f88105af038ebacad6edb205107c609b1
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-24 01:37:42 +0530

    maximum

---
+++ src/Array/maximum.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class maximum {
+    public static void main(String[] args) {
+        int[] arr = {12, 45, 67,13, 34};
+        int max = arr[0];
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
