7e6dc8683e056b3a0576c3cfa9bbef2c8caf1e10
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-25 12:27:03 +0530

    swap

--- src/Array/secondLarge.java
+++ src/Array/secondLarge.java
@@ -2,23 +2,17 @@ package Array;
 
 public class secondLarge {
     public static void main(String[] args) {
-
         int[] arr = {3, 4, 7, 9};
-
         int largest = Integer.MIN_VALUE;
         int secondLargest = Integer.MIN_VALUE;
-
         for (int i = 0; i < arr.length; i++) {
-
             if (arr[i] > largest) {
                 secondLargest = largest;
                 largest = arr[i];
-
             } else if (arr[i] > secondLargest && arr[i] != largest) {
                 secondLargest = arr[i];
             }
         }
-
         System.out.println(secondLargest);
     }
 }
