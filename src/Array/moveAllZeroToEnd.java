ddc93cf90feb885d9e4ebbf3db0e9dbffe950500
ROHIT SINGH <rohitsi2252@gmail.com>
2026-08-29 17:06:29 +0530

    push zero to end

---
+++ src/Array/moveAllZeroToEnd.java
@@ -0,0 +1,23 @@
+package Array;
+
+import java.util.Arrays;
+
+public class moveAllZeroToEnd {
+    public static void main(String[] args) {
+     int[] arr ={1, 2, 0, 4, 3, 0, 5, 0};
+        pushZerosToEnd(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void pushZerosToEnd(int[] arr) {
+       int j =0;
+       for(int i=0; i<arr.length; i++){
+           if(arr[i] != 0){
+               int temp  = arr[i];
+               arr[i] = arr[j];
+               arr[j] = temp;
+
+               j++;
+           }
+       }
+    }
+}
